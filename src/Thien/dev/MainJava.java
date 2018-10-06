b1be7797ca6e2c56e28cc39b71a367abdeb5d33c
thientran1199 <thientran1111999@gmail.com>
2018-10-06 08:50:01 +0700

    first commit

---
+++ src/Thien/dev/MainJava.java
@@ -0,0 +1,27 @@
+package Thien.dev;
+
+public class MainJava {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int a = 15;
+		int b = 6;
+		int tong = a+b;
+		int hieu = a-b;
+		int tich = a*b;
+		int thuong = a/b;
+		int phandu = a%b;
+		System.out.println(tong);
+	    System.out.println(hieu);
+	    System.out.println(tich);
+	    System.out.println(thuong);
+	    System.out.println(phandu);
+	    a++;
+	    System.out.println(a);
+	    a--;
+	    System.out.println(a);
+	    System.out.println(a>b);
+	    System.out.println(a<b);
+	    System.out.println(a==b);
+	    System.out.println(a!=b);
+	}
+}
